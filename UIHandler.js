ab7ca0f34df34cb7eb89c2cce5d8d6a1a834deda
Thanura Nadun <nadunnew@gmail.com>
2021-02-28 23:16:43 +0530

    this is not the best for but this may fine :)

--- UIHandler.js
+++ UIHandler.js
@@ -6,51 +6,54 @@ window.updateText = function (target,text){
     document.getElementById('popup').innerHTML = ""
 }
 
-window.popupShow = function (target,text){
+window.popupShow = function (target){
+    const targetObj = document.getElementById('w_'+target)
+    text = targetObj.innerText
     const word_list = window.spellchecker.suggest(text,10)
     let output_list = "<ul class='suggetion'>"
     for (const word of word_list) {
-        output_list += `<li onClick="window.updateText(${target},'${word}')"><span style="background-color: #FFFF00">${word}</span></li>`
+        output_list += `<li id="sug_${target}_${word}" ><span style="background-color: #FFFF00">${word}</span></li>`
     }
     output_list + "</ul>"
-
-    const targetObj = document.getElementById('w_'+target)
     const tooltip = document.getElementById('popup')
     tooltip.innerHTML = output_list
     popperJs.createPopper(targetObj,tooltip,{
         placement: 'bottom',
     })
+    for (const word of word_list) {
+        document.getElementById(`sug_${target}_${word}`).addEventListener("click",() => window.updateText(target,word))
+    }
 }
 
 window.render = function (target,word_array){
     let output = ""
+    console.log(word_array);
     for (const index in word_array) {
         var word = word_array[index]
-        if(word.includes(".")){
-            word = word.replace(".", "");
-            if (incorrectlist1.includes(word)){
-                position = incorrectlist1.indexOf(word);
-                output += `<span class="autocorrect">${correctlist1[position]}</span> `
-                output += '.'
-            }else if (!window.spellchecker.check(word)){
-                output += `<span class="invalid" id="w_${index}" onClick="window.popupShow(${index},'${word}')">${word}</span> `
-                output += '.'
-            }else{
-                output += word+'.'+' '
-            }
+        let haveDot = word.includes(".")
+        word = word.replace(".", "")
+        if (incorrectlist1.includes(word)){
+            position = incorrectlist1.indexOf(word);
+            output += `<span class="auto-correct">${correctlist1[position]}</span>`
+        }else if (!window.spellchecker.check(word)){
+            output += `<span class="invalid" id="w_${index}">${word}</span>`
         }else{
-            if (incorrectlist1.includes(word)){
-                position = incorrectlist1.indexOf(word);
-                output += `<span class="autocorrect">${correctlist1[position]}</span> `
-            }else if (!window.spellchecker.check(word)){
-                output += `<span class="invalid" id="w_${index}" onClick="window.popupShow(${index},'${word}')">${word}</span> `
-            }else{
-                output += word+' '
-            }
-        }  
+            output += word
+        }
+        if (haveDot) output += '.'
+        output += ' ' 
+    }
+    document.getElementById(target).innerHTML = output
+    for (const index in word_array) {
+        var word = word_array[index]
+        word = word.replace(".", "")
+        try {
+            document.getElementById(`w_${index}`).addEventListener("click",() => window.popupShow(index,word))
+        } catch (error) {
+            
+        }
     }
     window.spellchecktmp = {target,word_array}
-    document.getElementById(target).innerHTML = output
 }
 
 module.exports = window.render
